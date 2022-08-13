48186f1d764ca11fe0962b370bb9ad1c9de17a0b
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2022-08-13 22:18:04 +0530

    plotly

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import json
 
 
 # Add a title and intro text
-st.title('Earthquake Data Explorer')
-st.text('This is a web app to allow exploration of Earthquake Data')
+st.title('Predict the future performance of your suppliers :)')
+st.subheader('Type in the company name correctly')
 
 # Create file uploader object
-selected = st.text_input("", "Search...")
-button_clicked = st.button("OK")
+selected = st.text_input("", "")
+button_clicked = st.button("Click Here to Predict")
 print(button_clicked)
 
 # Check to see if a file has been uploaded
@@ -28,15 +28,33 @@ if button_clicked:
     res = dict(eval(x.text))
 
     # Create a section for matplotlib figure
-    st.header('Plot of Data')
+    st.header('Predicted Graph of %s'%(selected))
 
     x = res['Years']
     y = res['Performance']
 
     df = pd.DataFrame({"Year" : x, "Performance" : y})
     fig = go.Figure()
-    fig.add_scattergl(x=df['Year'], y=df['Performance'], line={'color': 'black'})
-    fig.add_scattergl(x=df.Year.where(df.Year>2020), y=df['Performance'], line={'color': 'red'})
+    fig.add_scattergl(x=df['Year'], y=df['Performance'], line={'color': 'blue'} , 
+    name = "Recorded Performance" , text= "Performance")
+    fig.add_scattergl(x=df.Year.where(df.Year>2020), y=df['Performance'], line={'color': 'pink'}, 
+    name = "Predicted Performance", text = "Predicted Performance")
+
+    fig.update_layout(
+    autosize=False,
+    width=500,
+    height=500,
+    margin=dict(
+        l=50,
+        r=50,
+        b=100,
+        t=100,
+        pad=4
+    ),
+    paper_bgcolor="LightSteelBlue",
+    )
+    
+
 
     
     st.plotly_chart(fig, use_container_width=True)
